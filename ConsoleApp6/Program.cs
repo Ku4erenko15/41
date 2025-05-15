b9d2c0e4765721a63416ab2f531125335264806b
Ku4erenko15 <159886914+Ku4erenko15@users.noreply.github.com>
2025-05-15 10:29:38 +0300

    Add files via upload

---
+++ ConsoleApp6/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp6
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            CompareDelegate minDelegate = GetMin;
+            CompareDelegate maxDelegate = GetMax;
+
+            double x = 7.47;
+            double y = 5.72;
+
+            Console.WriteLine("Минимум: " + minDelegate(x, y));
+            Console.WriteLine("Максимум: " + maxDelegate(x, y));
+            Console.Read();
+
+        }
+        static double GetMin(double a, double b)
+        {
+            return (a < b) ? a : b;
+        }
+
+        
+        static double GetMax(double a, double b)
+        {
+            return (a > b) ? a : b;
+        }
+
+       
+        delegate double CompareDelegate(double x, double y);
+
+    }
+}
